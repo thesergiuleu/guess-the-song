74a615b461f60b9919a21240de9e58fa6602f850
TheSergiuLeu <thesergiuleu@gmail.com>
2019-10-26 14:35:52 +0300

    protected route

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 Route::post('/register', 'Auth\AuthController@register');
 Route::post('/login', 'Auth\AuthController@login');
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:api']], function () {
+    Route::get('/', function () {
+        return 'Success';
+    });
 });
